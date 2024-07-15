5d12d523ff80fe443027ec55428cc16f760c0518
raul <raulmay8@hotmail.com>
2024-07-14 19:21:47 -0500

    mejoras al correo

--- GestionMedicoBackend/obj/Debug/net8.0/GestionMedicoBackend.AssemblyInfo.cs
+++ GestionMedicoBackend/obj/Debug/net8.0/GestionMedicoBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionMedicoBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09796d2755f42c7edd9201eaab6915e941861b84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f16c6828011361601a1dafdcc6c72d9f55bc0ecc")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionMedicoBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionMedicoBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
