5b38ffee2f773bc7e8222bcc3eb0249f23ce21fa
angelarias1 <106706081+angelarias1@users.noreply.github.com>
2024-07-15 19:07:26 -0500

    holi

--- GestionMedicoBackend/obj/Debug/net8.0/GestionMedicoBackend.AssemblyInfo.cs
+++ GestionMedicoBackend/obj/Debug/net8.0/GestionMedicoBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionMedicoBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df23046e40e9b90fd4cf41a9216452c2580ba9ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9acec6c8229239f5b5e3ca402090f05e140e18da")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionMedicoBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionMedicoBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
